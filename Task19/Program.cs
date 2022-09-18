a651fe13cae7c3a8b15a34d539518aea4b33e30d
AlexKozh <kojemiako-sascha@yandex.ru>
2022-09-18 14:50:46 +0700

    Solved problem 19

---
+++ Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine();
+Console.Write("Введите целое число:   ");
+string number = Console.ReadLine();
+int length = number.Length;
+
+bool IsPalindrome()
+{
+    for (int i = 0; i < length / 2; i++)
+        while (number[i] == number[length - 1])
+        {
+            return true;
+        }
+    return false;
+}
+
+Console.WriteLine("Ответ:");
+if (IsPalindrome()) Console.WriteLine($"1. Введенное число - {number} - является палиндромом");
+else Console.WriteLine($"1. Число {number} не является палиндромом");
+Console.WriteLine();
